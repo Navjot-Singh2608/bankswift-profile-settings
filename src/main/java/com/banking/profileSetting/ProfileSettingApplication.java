87ecd90263ae8b8e2a3c0a77149ea7e9047e37e2
navjotsingh2608 <navjot@argenelec.com>
2023-08-25 16:23:55 -0400

    first commit

---
+++ src/main/java/com/banking/profileSetting/ProfileSettingApplication.java
@@ -0,0 +1,13 @@
+package com.banking.profileSetting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileSettingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfileSettingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banking/profileSetting/ProfileSettingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.profileSetting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileSettingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
